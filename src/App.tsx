04714e70808592a4b2eba0100898e933c9e374fb
harryho <harry.ho_long@yahoo.com>
2019-12-30 15:12:59 +1100

    Test RoundedButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { RoundedButton } from './components/RoundedButton';
+
+const helloFunc = ()=> {
+  alert("Hello");
+}
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <RoundedButton color="maroon" onClick={helloFunc}>
+          New Button
+        </RoundedButton>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
